a15a9cce8656c1036f25561e0812e2082915e69f
Vimo <929992114@qq.com>
2019-01-31 02:42:08 +0800

    爬虫test

---
+++ TWSpider/TWSpider/spiders/mmav.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.http import Request
+from urllib import parse
+
+class MmavSpider(scrapy.Spider):
+    name = 'mmav'
+    allowed_domains = ['702rr.com']
+    start_urls = ['https://702rr.com/index/home.html']
+
+    def parse(self, response):
+        category = response.css('.row-item-content')
+        img_category = category[2]
+        img_url = img_category.css('a')
+        for node in img_url:
+            url = node.css('::attr(href)').extract_first('')
+            yield Request(url=parse.urljoin(response.url, url), callback=self.parse_detail)
+        pass
+
+    def parse_detail(self, response):
+        
+        pass
